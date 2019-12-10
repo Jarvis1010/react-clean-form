b601b3dcca3d475b5f7bf40353d77c6f0a75ce99
Jarvis1010 <travis.mair@gmail.com>
2019-12-10 01:31:03 -0700

    convert to hook and TS

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,7 @@
-import React from "react";
-import toJSON from "enzyme-to-json";
-import { shallow } from "enzyme";
-import Form from "./index";
+import React from 'react';
+import Form from './index';
 
-describe("Form functions", () => {
+describe('Form functions', () => {
   let wrapper;
   let spy;
   beforeEach(() => {
@@ -11,12 +9,12 @@ describe("Form functions", () => {
     wrapper = shallow(
       <Form
         initialState={{
-          input: "",
-          select: "",
+          input: '',
+          select: '',
           selectMultiple: [],
-          radio: "",
+          radio: '',
           checkbox1: false,
-          checkbox2: false
+          checkbox2: false,
         }}
         onSubmit={spy}
       >
@@ -35,101 +33,101 @@ describe("Form functions", () => {
         <input type="checkbox" name="checkbox2" value="checkbox2" /> checkbox2
         <button type="submit">Submit</button>
         {false && <p>null value</p>}
-      </Form>
+      </Form>,
     );
   });
-  it("Renders component", () => {
+  it('Renders component', () => {
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
-  it("calls custom onSubmit", () => {
+  it('calls custom onSubmit', () => {
     wrapper
-      .find("form")
+      .find('form')
       .first()
-      .simulate("submit");
+      .simulate('submit');
 
     expect(spy.mock.calls.length).toBe(1);
   });
-  it("updates value of input", () => {
-    const newValue = "Hello";
+  it('updates value of input', () => {
+    const newValue = 'Hello';
     wrapper
-      .find("form")
+      .find('form')
       .first()
-      .simulate("change", { target: { name: "input", value: newValue } });
+      .simulate('change', { target: { name: 'input', value: newValue } });
     expect(wrapper.state().input).toBe(newValue);
   });
-  it("updates value of select", () => {
-    const newValue = "value1";
+  it('updates value of select', () => {
+    const newValue = 'value1';
     wrapper
-      .find("form")
+      .find('form')
       .first()
-      .simulate("change", { target: { name: "select", value: newValue } });
+      .simulate('change', { target: { name: 'select', value: newValue } });
     expect(wrapper.state().select).toBe(newValue);
   });
-  it("adds value of select when multiple true", () => {
-    const newValue = "value1";
-    const newValue2 = "value2";
-    const form = wrapper.find("form").first();
-    form.simulate("change", {
+  it('adds value of select when multiple true', () => {
+    const newValue = 'value1';
+    const newValue2 = 'value2';
+    const form = wrapper.find('form').first();
+    form.simulate('change', {
       target: {
-        type: "select-multiple",
-        name: "selectMultiple",
-        value: newValue
-      }
+        type: 'select-multiple',
+        name: 'selectMultiple',
+        value: newValue,
+      },
     });
-    form.simulate("change", {
+    form.simulate('change', {
       target: {
-        type: "select-multiple",
-        name: "selectMultiple",
-        value: newValue2
-      }
+        type: 'select-multiple',
+        name: 'selectMultiple',
+        value: newValue2,
+      },
     });
     expect(wrapper.state().selectMultiple).toContain(newValue);
     expect(wrapper.state().selectMultiple).toContain(newValue2);
   });
-  it("removes value of select when multiple true", () => {
-    const newValue = "value1";
-    const newValue2 = "value2";
-    const form = wrapper.find("form").first();
-    form.simulate("change", {
+  it('removes value of select when multiple true', () => {
+    const newValue = 'value1';
+    const newValue2 = 'value2';
+    const form = wrapper.find('form').first();
+    form.simulate('change', {
       target: {
-        type: "select-multiple",
-        name: "selectMultiple",
-        value: newValue
-      }
+        type: 'select-multiple',
+        name: 'selectMultiple',
+        value: newValue,
+      },
     });
-    form.simulate("change", {
+    form.simulate('change', {
       target: {
-        type: "select-multiple",
-        name: "selectMultiple",
-        value: newValue2
-      }
+        type: 'select-multiple',
+        name: 'selectMultiple',
+        value: newValue2,
+      },
     });
-    form.simulate("change", {
+    form.simulate('change', {
       target: {
-        type: "select-multiple",
-        name: "selectMultiple",
-        value: newValue
-      }
+        type: 'select-multiple',
+        name: 'selectMultiple',
+        value: newValue,
+      },
     });
 
     expect(wrapper.state().selectMultiple).toContain(newValue2);
     expect(wrapper.state().selectMultiple).not.toContain(newValue);
   });
-  it("Keeps value of radio buttons", () => {
+  it('Keeps value of radio buttons', () => {
     const radios = wrapper.find('input[type="radio"]');
-    radios.first().simulate("click");
-    expect(radios.first().props().value).toBe("radio1");
-    expect(radios.last().props().value).toBe("radio2");
+    radios.first().simulate('click');
+    expect(radios.first().props().value).toBe('radio1');
+    expect(radios.last().props().value).toBe('radio2');
   });
-  it("Checks the box", () => {
-    const form = wrapper.find("form").first();
+  it('Checks the box', () => {
+    const form = wrapper.find('form').first();
 
-    form.first().simulate("change", {
+    form.first().simulate('change', {
       target: {
-        type: "checkbox",
-        name: "checkbox1",
-        checked: true
-      }
+        type: 'checkbox',
+        name: 'checkbox1',
+        checked: true,
+      },
     });
     expect(wrapper.state().checkbox1).toBe(true);
   });
